6761f4ede984fd066d9a39e1bdae4d9fccf89350
muhmd-ahmed <momo200200mm@gmail.com>
2022-03-02 18:26:25 +0200

    Final Design_final

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Location, LocationStrategy } from '@angular/common';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+ 
+  title = 'final';
+  active="listing";
+  role=0
+
+}
