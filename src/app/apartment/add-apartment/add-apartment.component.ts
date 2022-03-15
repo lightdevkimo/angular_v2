aeefbca94362c6590c694a0a5a66c856e02966bd
mahm0ud-abdelhalim <mahmoud.abdelhalim.mostafa@outlook.com>
2022-03-15 20:18:00 +0200

    improving error messages

--- src/app/apartment/add-apartment/add-apartment.component.ts
+++ src/app/apartment/add-apartment/add-apartment.component.ts
@@ -1,83 +1,94 @@
-import { img,user_info } from './../../_models/user.model';
+import { img, user_info } from './../../_models/user.model';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { apart,cities } from 'src/app/_models/user.model';
+import { apart, cities } from 'src/app/_models/user.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DataService } from 'src/app/_services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addApartment',
   templateUrl: './add-apartment.component.html',
-  styleUrls: ['./add-apartment.component.scss']
+  styleUrls: ['./add-apartment.component.scss'],
 })
 export class AddApartmentComponent implements OnInit {
-  user_info:user_info
+  user_info: user_info;
 
-  imagesUrl !: File
-  img!: any
+  imagesUrl!: File;
+  img!: any;
+  error = [];
 
   gov: cities[] = [];
   cities: cities[] = [];
   test = true;
 
-  constructor(private http: HttpClient, private data: DataService) { }
-
+  constructor(
+    private http: HttpClient,
+    private data: DataService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getcities();
   }
 
-
   getcities() {
-    this.http.get('http://127.0.0.1:8000/api/governates').subscribe(data => {
-
+    this.http.get('http://127.0.0.1:8000/api/governates').subscribe((data) => {
       for (let i = 0; i < data['data'].length; i++) {
-        this.gov[i] = (data['data'][i]);
+        this.gov[i] = data['data'][i];
       }
     });
-
   }
 
-
-
   choosegov(event: any) {
-    this.http.get('http://127.0.0.1:8000/api/findcities/'.concat(event.target.value)).subscribe(data => {
-      for (let i = 0; i < data['data'].length; i++) {
-        this.cities[i] = (data['data'][i]);
-      }
-    });
+    this.http
+      .get('http://127.0.0.1:8000/api/findcities/'.concat(event.target.value))
+      .subscribe((data) => {
+        for (let i = 0; i < data['data'].length; i++) {
+          this.cities[i] = data['data'][i];
+        }
+      });
     this.test = true;
   }
 
-
-
-
   addApartment(data: any) {
-
-    let db = new FormData()
+    let db = new FormData();
 
     for (const key in data) {
-
-      db.append(key, data[key])
+      db.append(key, data[key]);
     }
     console.log(this.imagesUrl);
 
     // let owner_id:any= 1;
-    db.append('images', this.imagesUrl)
-    db.append('owner_id', JSON.parse(localStorage.getItem('user_info'))['id'])
-    db.append('city_id', data['state'])
+    db.append('images', this.imagesUrl);
+    db.append('owner_id', JSON.parse(localStorage.getItem('user_info'))['id']);
+    db.append('city_id', data['state']);
 
-    this.http.post('http://127.0.0.1:8000/api/apartements', db,{ headers: new HttpHeaders().append('Authorization','Bearer '+localStorage.getItem('token'))}).subscribe(data => {
-      console.log(data);
-    }, error => {
-      console.log(error);
-    });
-
+    this.http
+      .post('http://127.0.0.1:8000/api/apartements', db, {
+        headers: new HttpHeaders().append(
+          'Authorization',
+          'Bearer ' + localStorage.getItem('token')
+        ),
+      })
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.router.navigateByUrl('/profile');
+        },
+        (err) => {
+          this.error=[]
+          for (const e in err.error.errors) {
+            // console.log(err.error.errors[e]);
+            
+            this.error.push( err.error.errors[e]);
+          }
+        }
+      );
   }
 
   selectFiles(event): void {
-    this.imagesUrl = event.target.files[0]
+    this.imagesUrl = event.target.files[0];
     console.log(this.imagesUrl);
   }
-
 }
